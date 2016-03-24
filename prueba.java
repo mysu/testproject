d7865104f440f18f08114036964460d094f54b3f
nicolas <nicolas.alvatri@gmail.com>
2016-03-24 17:42:28 -0500

    adicion de nuevas lineas

--- prueba.java
+++ prueba.java
@@ -4,8 +4,12 @@ import java.util.Scanner ;
 public class test {
 	public static void main(String[] args){
 	int edad;
+	String nombre ;
 	Scanner print = new Scanner (System.in) ;
-	edad=print.nextInt();
-	System.out.println("la edad de nicolas es " + edad);
+	System.out.println("Cual es su nombre ") ;
+		nombre= print.next();
+	System.out.println("Digite la edad ")
+		edad=print.nextInt();
+	System.out.println("la edad de "+nombre+" es " + edad);
 	}
 }
