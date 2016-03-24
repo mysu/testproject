bc83de85fee23caed0c265489edb405adb9a059e
nicolas <nicolas.alvatri@gmail.com>
2016-03-24 17:15:46 -0500

    mensaje mas la lectura de nombre

--- prueba.java
+++ prueba.java
@@ -1,8 +1,11 @@
-import bbla.co
 
+import java.util.Scanner ;
 //this is a test
 public class test {
-	public static void main
-	
-	System.out.println("Nicolas alvarez ")
+	public static void main(String[] args){
+	int edad;
+	Scanner print = new Scanner (System.in) ;
+	edad=pint.nextint();
+	System.out.println("la edad de nicolas es " + edad);
+	}
 }
