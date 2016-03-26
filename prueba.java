0611f3ec29b1778538310ee4f2c7f1b7cbfab3f1
Nicolas <nicolas.alvatri@gmail.com>
2016-03-26 15:27:56 -0500

    agregacion de suma

--- prueba.java
+++ prueba.java
@@ -3,8 +3,8 @@ import java.util.Scanner ;
 //this is a test
 public class prueba {
 	public static void main(String[] args){
-	int edad;
-	String nombre ;
+	int edad,numeros,n;
+	String nombre;
 	Scanner print = new Scanner (System.in) ;
 	System.out.println("Cual es su nombre ") ;
 		nombre= print.next();
@@ -12,7 +12,13 @@ public class prueba {
 		edad=print.nextInt();
 	System.out.println("la edad de "+nombre+"\n es " + edad );
 	}
-  
+    for (n=1;n<=10;n++){
+	Digite numeros
+	 numeros = print.nextInt();
+		n=n+numeros ;
+	}
+	System.out.println("la suma total de los numeros es "+n)
+	
   public static String LastName() {	
 	System.out.println("mostrar apellido");
     return "";
